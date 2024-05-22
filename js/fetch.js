2d63c635b735302dcd05722ff8e97a3642663e0f
Nidasakinaa <nidasakinaaulia@gmail.com>
2024-05-22 18:06:02 +0700

    update

--- js/fetch.js
+++ js/fetch.js
@@ -1,13 +1,25 @@
 import { get } from "https://bukulapak.github.io/api/process.js";
-import { setInner } from "https://bukulapak.github.io/element/process.js";
+import { addInner } from "https://bukulapak.github.io/element/process.js";
+import { getRandomColor, getRandomColorName } from "https://bukulapak.github.io/image/process.js";
+import { isiTabel } from "./table.js";
 let urlAPI = "https://ws-ats2024-d2eba2e3a16f.herokuapp.com/biodata";
-get(urlAPI,isiTableBiodata);
-function isiTableBiodata(results){
-    console.log(results);
+get(urlAPI, isiTableBiodata);
+function isiTableBiodata(results) {
     results.forEach(isiRow);
 }
-function isiRow(value){
-    console.log(value)
+function isiRow(value) {
+    let content = 
+    isiTabel.replace("#NAMA#", value.biodata.nama)
+            .replace("#PRODI#", value.biodata.prodi)
+            .replace("#NPM#", value.biodata.npm)
+            .replace("#KELAS#", value.biodata.kelas)
+            .replace("#ANGKATAN#", value.angkatan)
+            .replace("#JALAN#", value.jalan)
+            .replace("#KOTA#", value.biodata.kota)
+            .replace("#KODE POS#", value.biodata.kodepos)
+            .replace("#WARNA#", getRandomColor())
+            .replace(/#WARNALOGO#/g, getRandomColorName());
+        addInner("iniTabel", content);
 }
 
 
---
+++ js/table.js
@@ -0,0 +1,27 @@
+export let isiTabel = 
+`
+<tr class="h-18 border-b border-coolGray-100">
+    <th class="whitespace-nowrap px-4 bg-white text-left">
+        <div class="flex items-center -m-2">
+            <div class="w-auto p-2">
+                <input class="w-4 h-4 bg-white rounded" type="checkbox">
+            </div>
+            <div class="w-auto p-2">
+                <div class="flex items-center justify-center w-10 h-10 text-base font-medium text-#WARNALOGO#-600 bg-#WARNALOGO#-200 rounded-md">ULBI</div>
+            </div>
+            <div class="w-auto p-2">
+                <p class="text-xs font-semibold text-coolGray-800">#NAMA#</p>
+                <p class="text-xs font-medium text-coolGray-500">#NOHP#</p>
+            </div>
+        </div>
+    </th>
+    <th class="whitespace-nowrap px-4 bg-white text-sm font-medium text-coolGray-800 text-left">#NAMA#</th>
+    <th class="whitespace-nowrap px-4 bg-white text-sm font-medium text-coolGray-800 text-center">#PRODI#</th>
+    <th class="whitespace-nowrap px-4 bg-white text-sm font-medium text-coolGray-500 text-left">#NPM#</th>
+    <th class="whitespace-nowrap px-4 bg-white text-sm font-medium text-coolGray-500 text-left">#KELAS#</th>
+    <th class="whitespace-nowrap px-4 bg-white text-sm font-medium text-coolGray-500 text-left">#ANGKATAN#</th>
+    <th class="whitespace-nowrap px-4 bg-white text-sm font-medium text-#col#-500 text-left">#JALAN#</th>
+    <th class="whitespace-nowrap px-4 bg-white text-sm font-medium text-#col#-500 text-left">#KOTA#</th>
+    <th class="whitespace-nowrap px-4 bg-white text-sm font-medium text-#col#-500 text-left">#KODE POS#</th>
+</tr>
+`
