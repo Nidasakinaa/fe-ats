8480180f79d8efad8accf541a427063243dc9dfb
hrisz <harissaefuloh@gmail.com>
2024-05-23 11:06:42 +0700

    fix wrong directory

--- js/fetch.js
+++ js/fetch.js
@@ -1,5 +1,5 @@
 import { get } from "https://bukulapak.github.io/api/process.js";
-import { isiTableBiodata } from "./controller/get.js";
+import { isiTableBiodata } from "./controlller/get.js";
 import { urlAPI } from "./config/url.js";
 get(urlAPI, isiTableBiodata);
 
